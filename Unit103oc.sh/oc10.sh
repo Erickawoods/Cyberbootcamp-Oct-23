b19825a82c9e22466e0d27b90221aaf943838fa2
Ericka Woods <woods.ericka.r@gmail.com>
2023-11-21 17:25:25 -0500

    oc10.sh

--- Unit103oc.sh/oc10.sh
+++ Unit103oc.sh/oc10.sh
@@ -1,7 +1,14 @@
-count=1
-echo "ping -t 10.0.0.30"
-while [ $count ]; do
-        ping 10.0.0.30 -c 1
-        echo "$count"
-        ((count++))
+#!/bin/bash
+# Lets create an until loop that takes a variable and adds 1 till it reachs 10
+# Have the script echo out what are current number is
+
+counter=0
+
+echo "Enter a number between 1 & 10"
+read $number
+until [ "$counter" -ge 10 ]; do
+        echo "Counter: $counter"
+        ((counter++))
         done
+
+        echo "Loop done"
