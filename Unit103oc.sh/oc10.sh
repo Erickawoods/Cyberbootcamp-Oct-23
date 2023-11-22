a400890e0f424ee1775c93f8626730d71032774f
Ericka Woods <woods.ericka.r@gmail.com>
2023-11-21 20:05:16 -0500

    oc10.sh

--- Unit103oc.sh/oc10.sh
+++ Unit103oc.sh/oc10.sh
@@ -3,11 +3,8 @@
 # Have the script echo out what are current number is
 
 counter=0
-
-echo "Enter a number between 1 & 10"
-read $number
 until [ "$counter" -ge 10 ]; do
-        echo "Counter: $counter"
+        echo "Current number: $counter"
         ((counter++))
         done
 
