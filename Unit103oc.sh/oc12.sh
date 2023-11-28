bd73c9e29ddf9b6cbc19418d2d5933ff9943c4ab
Ericka Woods <woods.ericka.r@gmail.com>
2023-11-28 12:14:56 -0500

    oc12.sh

---
+++ Unit103oc.sh/oc12.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Today we are going to use a case statment instead of a conditional
+# Have the program ask how your day is and echo out a response for good or bad
+# Case statment format is a little different so please look up how this would be formated
+
+
+echo -n "How is your day? Enter; Good, Bad, or Ok."
+read -r answer
+
+echo -n "My day is $answer"
+
+case $answer in
+
+  Good)
+    echo -n "Good"
+    ;;
+
+  Ok)
+    echo -n "ok"
+    ;;
+
+  Bad)
+    echo -n "Bad"
+    ;;
+
+  esac
+
